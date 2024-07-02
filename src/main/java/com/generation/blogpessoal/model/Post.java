ae264b0ce1d44e3033f88d01820e3b94d35af50c
DouglasIde <douglasymide@gmail.com>
2024-07-02 15:18:36 -0300

    Criando a Classe POST na Camada MODEL

---
+++ src/main/java/com/generation/blogpessoal/model/Post.java
@@ -0,0 +1,12 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+
+
+public class Post{
+	
+}
+
