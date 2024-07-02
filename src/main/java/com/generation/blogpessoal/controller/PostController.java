ad91f5a7db0a0a372c55a011569b023b92816664
DouglasIde <douglasymide@gmail.com>
2024-07-02 15:40:16 -0300

    Criando o método GETALL na classe PostController

--- src/main/java/com/generation/blogpessoal/controller/PostController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostController.java
@@ -3,10 +3,13 @@ package com.generation.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.blogpessoal.model.Post;
 import com.generation.blogpessoal.repository.IPostRepository;
 
 
@@ -17,4 +20,9 @@ public class PostController {
 	
 	@Autowired
 	private IPostRepository postagemRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Post>> getAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
 }
