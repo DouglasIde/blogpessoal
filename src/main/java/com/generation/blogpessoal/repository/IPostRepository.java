af3e4a2aa0b10b18ad655b5a2bdd5ab1445f57d9
DouglasIde <douglasymide@gmail.com>
2024-07-03 13:56:43 -0300

    Implementando o Método de Consulta

--- src/main/java/com/generation/blogpessoal/repository/IPostRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/IPostRepository.java
@@ -1,9 +1,12 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Post;
 
 public interface IPostRepository extends JpaRepository<Post, Long> {
-
+	public List <Post> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
